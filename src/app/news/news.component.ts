875239efe2b592dd50aece273a27592c8df1bf07
manoj_pathak <manoj.pathak1215@gmail.com>
2018-01-19 09:39:57 +0530

    swipe events

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+    let hammertime = new Hammer(document.getElementById(''));
+    hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 
-  ngOnInit() {
-  }
+    news = [];
+    selectedIndex:number = 0;
+    constructor() { }
+
+    ngOnInit() {
+        this.news = JSON.parse(localStorage.getItem('News'));
+    }
+
+
+    SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight'};
+
+
+    // action triggered when user swipes
+    swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT) {
+
+    // swipe right, next avatar
+    if (action === this.SWIPE_ACTION.RIGHT) {
+        const isLast = currentIndex === this.news.length - 1;
+        this.selectedIndex = isLast ? 0 : currentIndex + 1;
+        console.log(this.selectedIndex);
+    }
+
+    // swipe left, previous avatar
+    if (action === this.SWIPE_ACTION.LEFT) {
+        const isFirst = currentIndex === 0;
+        this.selectedIndex = isFirst ? this.news.length - 1 : currentIndex - 1;
+        console.log(this.selectedIndex);
+    }
+
+    }
 
 }
--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -11,6 +11,7 @@ export class ProfileService {
             .subscribe(res => {
                 if(res.status==="ok") {
                     vm.news = res.articles;
+                    localStorage.setItem('News', JSON.stringify(vm.news));
                     console.log(vm.news);
                     vm.viewIndex = 1;
                 }
